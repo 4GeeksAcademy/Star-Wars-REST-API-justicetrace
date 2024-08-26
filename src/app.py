86c5999b6adc3eca1e9761342e5f7db6ffc09c9b
Justice Rivera <142175716+justicetrace@users.noreply.github.com>
2024-08-26 15:45:58 +0000

    updated app.py file

--- src/app.py
+++ src/app.py
@@ -56,9 +56,65 @@ def get_person(people_id):
         raise APIException("person not found", status_code=404)
     return jsonify(person.serialized()), 200
 
-# [GET] /people/<int:people_id> Get one single person's information.
-# [GET] /planets Get a list of all the planets in the database.
-# [GET] /planets/<int:planet_id> Get one single planet's information.
+@app.route('/planet', methods=['GET'])
+def list_planet():
+    planet = Planet.query.all()
+    serialized_planet = [planet.serialize() for planet in planet]
+    return jsonify(serialized_planet), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        raise APIException("planet not found", status_code=404)
+    return jsonify(planet.serialized()), 200
+
+
+
+@app.route('/users/favorites', methods=['GET'])
+def list_user_favorites():
+    user_id = request.args.get('user_id')
+    user = User.query.get(user_id)
+    if user is None:
+        user = User.query.first()
+        if user is None:
+            raise APIException("User not found", status_code=404)
+    
+    favorites = Favorite.query.filter_by(user_id=user_id).all()
+    serialized_favorites = [favorite.serialize() for favorite in favorites]
+    return jsonify(serialized_favorites), 200
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user_id = request.args.get('user_id')
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException("User not found", status_code=404)
+    
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        raise APIException("Planet not found", status_code=404)
+    
+    favorite = Favorite(name=planet.name, user_id=user_id, planet_id=planet_id)
+    db.session.add(favorite)
+    db.session.commit()
+    
+    return jsonify({"message": "Favorite planet added successfully"}), 200
+
+@app.route('/favorite/<int:id>', methods=['DELETE'])
+def delete_favorite(id):
+    favorite = Favorite.query.get(id)
+    if not favorite:
+        return jsonify({"error": "Favorite not found"}), 404
+    
+    db.session.delete(favorite)
+    db.session.commit()
+    
+    return jsonify({"message": "Favorite deleted successfully"}), 200
+
+
+# [GET] /planets Get a list of all the planets in the database. *
+# [GET] /planets/<int:planet_id> Get one single planet's information. *
 # Additionally, create the following endpoints to allow your StarWars blog to have users and favorites:
 
 # [GET] /users/favorites Get all the favorites that belong to the current user.
